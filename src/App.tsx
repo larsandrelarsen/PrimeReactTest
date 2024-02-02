257f838bf65fcdab3392dab89fc008be1834fa75
lars.larsen <t5x7yj7twoouiajir3m22v32r7d7pqte372rte7no55ocfobycvq>
2024-02-02 07:18:22 +0100

    int

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { PrimeReactProvider } from 'primereact/api';
+import { Button } from 'primereact/button';
+import { Accordion, AccordionTab } from 'primereact/accordion';
+import { Ripple } from 'primereact/ripple';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+function App() {
+  const value = {
+    ripple: true
+  };
+
+  return (
+    <div className="App">
+      <PrimeReactProvider value={value}>
+        <h1>Inside of PrimeReactProvider</h1>
+        <Button className="p-ripple" label="Submit">
+          <Ripple />
+        </Button>
+        <Accordion activeIndex={0}>
+          <AccordionTab header="Header I">
+            <p className="m-0">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </AccordionTab>
+          <AccordionTab header="Header II">
+            <p className="m-0">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+              sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+              Consectetur, adipisci velit, sed quia non numquam eius modi.
+            </p>
+          </AccordionTab>
+          <AccordionTab header="Header III">
+            <p className="m-0">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+              quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt
+              mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+              Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+            </p>
+          </AccordionTab>
+        </Accordion>
+      </PrimeReactProvider>
+    </div>
+  );
+}
+
+export default App;
