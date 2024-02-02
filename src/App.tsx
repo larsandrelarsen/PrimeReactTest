ddd8b3fa507b3d8865e278b4c67378069f145311
lars.larsen <t5x7yj7twoouiajir3m22v32r7d7pqte372rte7no55ocfobycvq>
2024-02-02 12:13:17 +0100

    LadingPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import FaqBlock from './component/block/landingpage/faq/FaqBlock';
 import TopNav from './component/nav/TopNav';
 import Hero from './component/block/landingpage/hero/Hero';
 import FooterHero from './component/block/landingpage/hero/FooterHero';
+import FooterNav from './component/nav/FooterNav';
 
 function App() {
   const value = {
@@ -19,6 +20,7 @@ function App() {
         <EffectBlock />
         <FaqBlock />
         <FooterHero />
+        <FooterNav />
       </PrimeReactProvider>
     </div>
   );
